48cebbc17c4ef3898a8db91a5449cee3c8292931
20221tiimi0130 <dudinhafrancosantana@gmail.com>
2024-05-03 11:16:44 -0300

    Parei no passo 8

---
+++ app/src/main/java/franco/duda/lista/activity/MainActivity.java
@@ -0,0 +1,43 @@
+package franco.duda.lista.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import franco.duda.lista.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    static int NEW_ITEM_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.etDesc), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FloatingActionButton fabAddItem = findViewById(R.id.floatingActionButton);
+
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,NewItemActivity.class);
+                startActivityForResult(i, NEW_ITEM_REQUEST);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/franco/duda/lista/activity/NewItemActivity.java
@@ -0,0 +1,46 @@
+package franco.duda.lista.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import franco.duda.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
+
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_new_item);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.etDesc), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ImageButton imgCI = findViewById(R.id.imbCI);
+
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent,PHOTO_PICKER_REQUEST);
+            }
+        });
+    }
+
+
+}
